a03cba08d0fd27035e330a41ff1ad0199e77346d
Elkin Romero <gio.busrom@gmail.com>
2020-04-16 18:50:23 -0600

    cleanup

--- tethysapp/stream3d/public/js/geoglows.js
+++ tethysapp/stream3d/public/js/geoglows.js
@@ -1,2 +1,2 @@
 //create the same thing for the tethys app same logic baby
-// this will help us to see if we can export or not the things otherwise it will be fucking long 
+// this will help us to see if we can export or not the things otherwise it will be
